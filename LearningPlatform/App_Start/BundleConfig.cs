f4c49d24da491092dd69f69ce600c8c7d4840f20
Mihai Patrascoiu <patrascoiu.mihai@gmail.com>
2017-11-07 00:41:48 +0100

    Updated BundleConfig

--- LearningPlatform/App_Start/BundleConfig.cs
+++ LearningPlatform/App_Start/BundleConfig.cs
@@ -23,9 +23,11 @@ namespace LearningPlatform
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                      "~/Content/bootstrap.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                      "~/Content/*-style.css"));
         }
     }
 }
